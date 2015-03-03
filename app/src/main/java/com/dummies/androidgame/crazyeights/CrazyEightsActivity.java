fac0f473b96e60af84d55f125c648b34d1fdd043
Wes Reid <kohopolo@gmail.com>
2015-03-02 23:40:17 -0600

    Added Custom View (CrazyEightsView). Added ability to expand red dot by dragging finger across screen. Double tap to reset dot still not implemented.

--- app/src/main/java/com/dummies/androidgame/crazyeights/CrazyEightsActivity.java
+++ app/src/main/java/com/dummies/androidgame/crazyeights/CrazyEightsActivity.java
@@ -8,13 +8,21 @@ import android.view.WindowManager;
 public class CrazyEightsActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        TitleView tView = new TitleView(this);
-        tView.setKeepScreenOn(true);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        					 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(tView);
+
+        //Test using custom view
+        CrazyEightsView myView = new CrazyEightsView( this );
+        setContentView(myView);
+
+        //ACTUAL GAME VIEW
+//        TitleView tView = new TitleView(this);
+//        tView.setKeepScreenOn(true);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//        					 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(tView);
     }
 }
--- app/src/main/java/com/dummies/androidgame/crazyeights/CrazyEightsView.java
+++ app/src/main/java/com/dummies/androidgame/crazyeights/CrazyEightsView.java
@@ -29,27 +29,52 @@ public class CrazyEightsView extends View {
 		 canvas.drawCircle(circleX, circleY, radius, redPaint);
 	 }
 	 
-	 public boolean onTouchEvent(MotionEvent event) {
-	        int eventaction = event.getAction();   
-	        int X = (int)event.getX();
-	        int Y = (int)event.getY();
+//	 public boolean onTouchEvent(MotionEvent event) {
+//	        int eventaction = event.getAction();
+//	        int X = (int)event.getX();
+//	        int Y = (int)event.getY();
+//
+//	        switch (eventaction ) {
+//
+//	        case MotionEvent.ACTION_DOWN:
+//	        	break;
+//
+//	        case MotionEvent.ACTION_MOVE:
+//	        	break;
+//
+//	        case MotionEvent.ACTION_UP:
+//	        	circleX = X;
+//	        	circleY = Y;
+//	        	break;
+//	        }
+//	        invalidate();
+//			return true;
+//	}
 
-	        switch (eventaction ) {
+    public boolean onTouchEvent( MotionEvent event )
+    {
+        int eventaction = event.getAction();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
 
-	        case MotionEvent.ACTION_DOWN: 
-	        	break;
-
-	        case MotionEvent.ACTION_MOVE: 
-	        	break;
-	        	
-	        case MotionEvent.ACTION_UP:
-	        	circleX = X;
-	        	circleY = Y;
-	        	break;
-	        } 
-	        invalidate();
-			return true;
-	        
-	}
+        switch (eventaction )
+        {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //grow radius while dragging finger on screen
+                radius += 1;
+                break;
+            case MotionEvent.ACTION_UP:
+                circleX = x;
+                circleY = y;
+                break;
+            case MotionEvent.ACTION_POINTER_2_UP: //doesn't work
+                radius = 30;
+                break;
+        }
+        invalidate();
+        return true;
+    }
 	 
 }
